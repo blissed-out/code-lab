c9bb8414b1f1f1e9b2beb3fa0b1de8000b8660b2
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-06 15:29:10 +0545

    add: proper import of validators

--- backend/src/routers/auth.route.js
+++ backend/src/routers/auth.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { register } from "../controllers/auth.controller.js";
+import { login, register } from "../controllers/auth.controller.js";
 import { registrationValidation, loginValidation } from "../utils/validator.js";
+import validate from "../middlewares/validation.middleware.js";
 
 const authRoute = Router();
 
-authRoute.get("/register", registrationValidation, register);
+authRoute.get("/register", registrationValidation, validate, register);
+authRoute.get("/login", loginValidation, validate, login);
 
 export default authRoute;
