24cbb2f6a8add625a887416674df4c89fa54b1c9
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-06 15:30:00 +0545

    add: validation middleware to throw error

---
+++ backend/src/middlewares/validation.middleware.js
@@ -0,0 +1,16 @@
+import { validationResult } from "express-validator";
+import ApiError from "../utils/api-error";
+
+const validate = (next) => {
+    const result = validationResult("body");
+    if (result.isEmpty()) {
+        next();
+    } else {
+        console.log("Errors: ", result.array());
+        res.status(401).json(
+            new ApiError(401, "Validation Error", result.array())
+        );
+    }
+};
+
+export default validate;
