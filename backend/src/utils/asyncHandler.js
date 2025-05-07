59324f58893f29876a0dd8616027e94b0e3b885a
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-07 21:29:16 +0545

    add: proper error logging

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,17 +1,14 @@
 import ApiResponse from "./api-response.js";
 
 const asyncHandler = (requestHandler) => async (req, res, next) => {
-    try {
-        await requestHandler(req, res);
-        console.log("this is working well for now");
-        console.log("this is working well for now, isn't is so sir?");
-    } catch (error) {
-        res.status(error.code).json(
-            new ApiResponse(error.code, null, error.message)
-        );
-    }
+  try {
+    await requestHandler(req, res);
+  } catch (error) {
+    console.error("Error", error)
+    res.status(error.code).json(
+      new ApiResponse(error.code, null, error.message)
+    );
+  }
 };
 
 export default asyncHandler;
-
-console.log("this is working well for now");
