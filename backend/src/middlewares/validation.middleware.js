167c0e7a6095cce0db088543fe780e14a566bd8f
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-07 08:34:51 +0545

    chore: update packages

--- backend/src/middlewares/validation.middleware.js
+++ backend/src/middlewares/validation.middleware.js
@@ -1,16 +1,13 @@
 import { validationResult } from "express-validator";
-import ApiError from "../utils/api-error.js";
+import ApiResponse from "../utils/api-response.js";
 
 const validate = (next) => {
-    const result = validationResult("body");
-    if (result.isEmpty()) {
-        next();
-    } else {
-        console.log("Errors: ", result.array());
-        res.status(401).json(
-            new ApiError(401, "Validation Error", result.array())
-        );
-    }
-};
+  const result = validationResult("body");
+  if (result.isEmpty()) {
+    next();
+  } else {
+    console.log("Errors: ", result.array());
+  };
+}
 
 export default validate;
--- backend/src/utils/validator.js
+++ backend/src/utils/validator.js
@@ -1,50 +1,50 @@
-import { body, params } from "express-validator";
+import { body } from "express-validator";
 
 const registrationValidation = async (req, res) => {
-    return [
-        body("email")
-            .trim()
-            .notEmpty()
-            .withMessage("email field is required")
-            .isEmail()
-            .withMessage("invalid email"),
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("email field is required")
+      .isEmail()
+      .withMessage("invalid email"),
 
-        body("username")
-            .trim()
-            .notEmpty()
-            .withMessage("username field is required")
-            .isLength({ min: 3 })
-            .withMessage("username must be atleast 3 characters")
-            .isLength({ max: 12 })
-            .withMessage("username must not exceed 12 characters"),
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("username field is required")
+      .isLength({ min: 3 })
+      .withMessage("username must be atleast 3 characters")
+      .isLength({ max: 12 })
+      .withMessage("username must not exceed 12 characters"),
 
-        body("password")
-            .trim()
-            .notEmpty()
-            .withMessage("password field is required")
-            .isLength({ min: 6 })
-            .withMessage("password must be atleast 6 characters long"),
-        // no max requirement for now
-    ];
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("password field is required")
+      .isLength({ min: 6 })
+      .withMessage("password must be atleast 6 characters long"),
+    // no max requirement for now
+  ];
 };
 
 const loginValidation = () => {
-    return [
-        body("email")
-            .trim()
-            .notEmpty()
-            .withMessage("email field is required")
-            .isEmail()
-            .withMessage("invalid email"),
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("email field is required")
+      .isEmail()
+      .withMessage("invalid email"),
 
-        body("password")
-            .trim()
-            .notEmpty()
-            .withMessage("password field is required")
-            .isLength({ min: 6 })
-            .withMessage("password must be atleast 6 characters long"),
-        // no max requirement for now
-    ];
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("password field is required")
+      .isLength({ min: 6 })
+      .withMessage("password must be atleast 6 characters long"),
+    // no max requirement for now
+  ];
 };
 
 export { registrationValidation, loginValidation };
