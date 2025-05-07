c5a5ae14111d269b79e6eb37b618502092b5f31b
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-07 07:14:26 +0545

    add: throw api response

--- backend/src/middlewares/validation.middleware.js
+++ backend/src/middlewares/validation.middleware.js
@@ -1,5 +1,5 @@
 import { validationResult } from "express-validator";
-import ApiError from "../utils/api-error";
+import ApiError from "../utils/api-error.js";
 
 const validate = (next) => {
     const result = validationResult("body");
