577e07b240c7bd3f541dc1ed19bb559199119e2d
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-07 21:26:58 +0545

    fix: import correctly

--- backend/src/libs/db.js
+++ backend/src/libs/db.js
@@ -1,7 +1,7 @@
-import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaClient } from "../generated/prisma/client.js";
 
 const globalForPrisma = globalThis;
 
-export const db = globalForPrisma || new PrismaClient();
+export const db = new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.db = db;
