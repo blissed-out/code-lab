2d15a5936860735fdfbd81bac9490622c1690b73
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-07 07:49:14 +0545

    add: cookie feature

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,13 @@
 import express from "express";
 import authRoute from "./routers/auth.route.js";
+import cookieParser from "cookie-parser"
 
 const app = express();
 
-// import all kind of middlewares to use project wise
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-// routes
 app.use("/api/v1/auth", authRoute);
 
 export default app;
