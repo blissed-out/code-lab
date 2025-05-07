511d9c7b0dada9cd89ce32b0edc51369afe15ab3
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-07 21:27:46 +0545

    fix: include res too, so next can work

--- backend/src/middlewares/validation.middleware.js
+++ backend/src/middlewares/validation.middleware.js
@@ -1,12 +1,14 @@
 import { validationResult } from "express-validator";
 import ApiResponse from "../utils/api-response.js";
 
-const validate = (next) => {
+const validate = (req, res, next) => {
+  console.log("validator to catch error running");
   const result = validationResult("body");
   if (result.isEmpty()) {
     next();
   } else {
     console.log("Errors: ", result.array());
+    res.status(401).json(new ApiResponse(401, null, result.array));
   };
 }
 
