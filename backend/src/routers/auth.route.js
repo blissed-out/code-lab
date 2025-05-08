de8ca53018dd2faaf6b363dd4ac5b13526e89ad8
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-08 15:32:14 +0545

    fix: import login middleware + change logout to post

--- backend/src/routers/auth.route.js
+++ backend/src/routers/auth.route.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { login, register, logout } from "../controllers/auth.controller.js";
 import { registrationValidation, loginValidation } from "../utils/validator.js";
 import validate from "../middlewares/validation.middleware.js";
+import isLoggedIn from "../middlewares/login.middleware.js";
 
 const authRoute = Router();
 
 authRoute.post("/register", registrationValidation(), validate, register);
 authRoute.post("/login", loginValidation(), validate, login);
-authRoute.post("/logout", validate, logout);
+authRoute.post("/logout", isLoggedIn, validate, logout);
 
 export default authRoute;
