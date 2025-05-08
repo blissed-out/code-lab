86fea3b67c0d02a8d7070801e706ad8827c31be8
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-08 15:52:15 +0545

    fix: logic improve

--- backend/src/middlewares/login.middleware.js
+++ backend/src/middlewares/login.middleware.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken"
 import ApiResponse from "../utils/api-response.js";
+import { db } from "../libs/db.js";
 const isLoggedIn = (req, res, next) => {
   const token = req.cookies.token;
 
@@ -9,9 +10,20 @@ const isLoggedIn = (req, res, next) => {
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY, { algorithms: "HS256" });
 
-  if (!decoded) {
-    res.status(401).json(new ApiResponse(401, null, "invalid token"));
+  // if (!decoded) {
+  //   res.status(401).json(new ApiResponse(401, null, "invalid token"));
+  // }
+  const user = db.user.findUnique({
+    where: {
+      id: decoded.id,
+    },
+  })
+
+  if (!user) {
+    return res.status(404).json(new ApiResponse(404, null, "user not found"))
   }
+
+  // req.user = user;
   next();
 }
 
