416bad3dbdaed1349df05b0e43e619de900c5894
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-12 15:13:49 +0545

    add: submit results in batch function

--- backend/src/libs/judge0.lib.js
+++ backend/src/libs/judge0.lib.js
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 export const getJudge0LangagueId = (language) => {
   const languages = {
     "python": 71,
@@ -5,4 +7,12 @@ export const getJudge0LangagueId = (language) => {
     "java": 62
   }
   return languages[language.toLowerCase()]
+}
+
+export const submitBatch = async (submissions) => {
+  const { data } = axios.post(`${process.env.JUDGE0_API}/submissions/batch?base64_encoded=false`, submissions);
+
+  console.log("submitBatch data: ", data);
+
+  return data // arrays of token
 }
