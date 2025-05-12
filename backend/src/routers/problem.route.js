eaefe55be0c86aa165780d51a6ec7d313f5f7658
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-12 20:05:15 +0545

    fix: import controllers properly

--- backend/src/routers/problem.route.js
+++ backend/src/routers/problem.route.js
@@ -1,17 +1,19 @@
 import { Router } from "express";
-import { isAdmin, isLoggedIn } from "../middlewares/login.middleware";
-import { createProblem } from "../controllers/problem.controller";
+import { isAdmin, isLoggedIn } from "../middlewares/login.middleware.js";
+import { createProblem, getAllProblems, getProblem, updateProblem, deleteProblem, getAllSolvedProblemsByUser } from "../controllers/problem.controller.js";
 
-const route = Router()
+const authRoute = Router()
 
-route.post("/create-problem", isLoggedIn, isAdmin, createProblem)
+authRoute.post("/create-problem", isLoggedIn, isAdmin, createProblem)
 
-route.get("/get-all-problems", isLoggedIn, getAllProblems)
+authRoute.get("/get-all-problems", isLoggedIn, getAllProblems)
 
-route.get("/get-problem/:id", isLoggedIn, getProblem)
+authRoute.get("/get-problem/:id", isLoggedIn, getProblem)
 
-route.post("/update-problem/:id", isLoggedIn, isAdmin, updateProblem)
+authRoute.post("/update-problem/:id", isLoggedIn, isAdmin, updateProblem)
 
-route.delete("/delete-problem/:id", isLoggedIn, isAdmin, deleteProblem)
+authRoute.delete("/delete-problem/:id", isLoggedIn, isAdmin, deleteProblem)
 
-route.get("/get-solved-problem", isLoggedIn, getSolvedProblem)
+authRoute.get("/get-solved-problem", isLoggedIn, getAllSolvedProblemsByUser)
+
+export default authRoute
