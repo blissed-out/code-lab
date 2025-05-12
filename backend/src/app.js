98bde31e8bcbb6714c9d63bbc2712a1632a7610f
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-12 20:04:16 +0545

    fix: problem route minor mistake

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoute from "./routers/auth.route.js";
+import problemRoute from "./routers/problem.route.js"
 import cookieParser from "cookie-parser"
 
 const app = express();
@@ -9,5 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/problem", problemRoute);
 
 export default app;
