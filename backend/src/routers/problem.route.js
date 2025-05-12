3b5ac01b359e321de03526615860d1c15bcfd594
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-12 12:34:39 +0545

    add: problem routes

---
+++ backend/src/routers/problem.route.js
@@ -0,0 +1,46 @@
+import { Router } from "express";
+import { isAdmin, isLoggedIn } from "../middlewares/login.middleware";
+import { createProblem } from "../controllers/problem.controller";
+
+const route = Router()
+
+route.post("/create-problem", isLoggedIn, isAdmin, createProblem)
+
+route.get("/get-all-problems", isLoggedIn, getAllProblems)
+
+route.get("/get-problem/:id", isLoggedIn, getProblem)
+
+route.post("/update-problem/:id", isLoggedIn, isAdmin, updateProblem)
+
+route.delete("/delete-problem/:id", isLoggedIn, isAdmin, deleteProblem)
+
+route.get("/get-solved-problem", isLoggedIn, getSolvedProblem)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
