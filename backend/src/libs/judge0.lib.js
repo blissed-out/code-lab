23cb97c6ec586215a29cb472e43909828cf88f3e
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-12 17:14:18 +0545

    add: modular code for create problem controller

--- backend/src/libs/judge0.lib.js
+++ backend/src/libs/judge0.lib.js
@@ -10,9 +10,36 @@ export const getJudge0LangagueId = (language) => {
 }
 
 export const submitBatch = async (submissions) => {
+
   const { data } = axios.post(`${process.env.JUDGE0_API}/submissions/batch?base64_encoded=false`, submissions);
 
   console.log("submitBatch data: ", data);
 
   return data // arrays of token
+}
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const pollBatch = async (tokens) => {
+
+  while (true) {
+
+    const { data } = await axios.get(`${process.env.JUDGE0_API}/submissions/batch`, {
+      params: {
+        tokens: tokens.join(","),
+        base64_encoded: false
+      }
+    })
+
+    const results = data.submissions.status_id;
+
+    const isAllDone = results.every(results !== 1 && results !== 2);
+
+    if (isAllDone) {
+      return results
+    }
+
+    await sleep(1000); // wait for 1 seconds before hitting the endpoint again
+
+  }
 }
