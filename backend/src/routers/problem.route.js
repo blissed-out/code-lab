ab348cb32622ac33a1e2ed1484238572f653acc3
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-13 17:34:13 +0545

    add: get problem by id and get all problems

--- backend/src/routers/problem.route.js
+++ backend/src/routers/problem.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { isAdmin, isLoggedIn } from "../middlewares/login.middleware.js";
-import { createProblem, getAllProblems, getProblem, updateProblem, deleteProblem, getAllSolvedProblemsByUser } from "../controllers/problem.controller.js";
+import { createProblem, getAllProblems, getProblemById, updateProblem, deleteProblem, getAllSolvedProblemsByUser } from "../controllers/problem.controller.js";
 
 const authRoute = Router()
 
@@ -8,7 +8,7 @@ authRoute.post("/create-problem", isLoggedIn, isAdmin, createProblem)
 
 authRoute.get("/get-all-problems", isLoggedIn, getAllProblems)
 
-authRoute.get("/get-problem/:id", isLoggedIn, getProblem)
+authRoute.get("/get-problem/:id", isLoggedIn, getProblemById)
 
 authRoute.post("/update-problem/:id", isLoggedIn, isAdmin, updateProblem)
 
