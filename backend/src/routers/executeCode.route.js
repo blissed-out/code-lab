24f54db62624a4027d294c8e9b259976f1e0ac90
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-19 13:55:23 +0545

    add: execute route to execute code

--- backend/src/routers/executeCode.route.js
+++ backend/src/routers/executeCode.route.js
@@ -1,7 +1,9 @@
 import { Router } from "express"
+import { isLoggedIn } from "../middlewares/login.middleware.js";
+import { executeCode } from "../controllers/executeCode.controller.js";
 
 const executionRoute = Router();
 
-
+executionRoute.post("/", isLoggedIn, executeCode);
 
 export default executionRoute
