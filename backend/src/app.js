1dd85701585ce638e2c89980628e2e78444a872e
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-19 13:54:17 +0545

    add: submission and playlist route

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,8 @@ import authRoute from "./routers/auth.route.js";
 import problemRoute from "./routers/problem.route.js"
 import executionRoute from "./routers/executeCode.route.js";
 import cookieParser from "cookie-parser"
+import submissionRoute from "./routers/submission.route.js";
+import playlistRouter from "./routers/playlist.route.js";
 
 const app = express();
 
@@ -13,5 +15,7 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/problem", problemRoute);
 app.use("/api/v1/execute-code", executionRoute);
+app.use("/api/v1/submission", submissionRoute);
+app.use("/api/v1/playlist", playlistRouter);
 
 export default app;
