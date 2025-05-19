d62e2be75d13e02c4e67eac9f2bad1942738125a
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-19 13:57:22 +0545

    add: route to submit

---
+++ backend/src/routers/playlist.route.js
@@ -0,0 +1,20 @@
+import { Router } from "express"
+import { isLoggedIn } from "../middlewares/login.middleware.js";
+import { addProblemToPlaylist, createPlaylist, deletePlaylist, getAllListDetails, getPlayListDetails, removeProblemFromPlaylist } from "../controllers/playlist.controller.js";
+
+const playlistRouter = Router();
+
+playlistRouter.get("/", isLoggedIn, getAllListDetails);
+
+playlistRouter.get("/:playlistId", isLoggedIn, getPlayListDetails)
+
+playlistRouter.post("/create-playlist", isLoggedIn, createPlaylist);
+
+playlistRouter.post("/:playlistId/add-problem", isLoggedIn, addProblemToPlaylist);
+
+playlistRouter.delete("/:playlistId", isLoggedIn, deletePlaylist);
+
+playlistRouter.delete("/:playlistId/remove-problem", isLoggedIn, removeProblemFromPlaylist);
+
+export default playlistRouter;
+
