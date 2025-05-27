30d22247b94222fa16a4f3bb8457ec6fba424cb4
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-27 06:52:41 +0545

    add: app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { Loader } from "lucide-react";
+
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import { useAuthStore } from "./store/useAuthStore";
 
 const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore;
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-sssscreen">
+        <Loader classname="size-10 animate-spin" />
+      </div>
+    );
+  }
+
   let authUser = null;
 
   return (
     <div className="flex flex-col items-center justify-start">
+      <Toaster />
       <Routes>
         <Route
           path="/"
