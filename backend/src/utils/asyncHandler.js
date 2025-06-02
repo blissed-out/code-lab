55a52a73e748cabdf67b9a2f22b8dd83f6865ec2
blissed-out <aadarshbanjade.computer@gmail.com>
2025-06-02 22:45:51 +0545

    fix: allow use of throw new api error

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,13 +1,18 @@
 import ApiResponse from "./api-response.js";
+import ApiError from "./api-error.js";
 
 const asyncHandler = (requestHandler) => async (req, res, next) => {
   try {
     await requestHandler(req, res, next);
   } catch (error) {
     console.error("Error", error);
-    res
-      .status(error.code)
-      .json(new ApiResponse(error.code, null, error.message));
+
+    if (error instanceof ApiError) {
+      return res
+        .status(error.statusCode)
+        .json(new ApiResponse(error.statusCode, null, error.message));
+    }
+    res.status(500).json(new ApiResponse(500, null, "Internal Server Error"));
   }
 };
 
