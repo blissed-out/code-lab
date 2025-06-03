5222707335cd3edca2ef7d06563cd780634ca810
blissed-out <aadarshbanjade.computer@gmail.com>
2025-06-03 20:22:04 +0545

    add: update profile route

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ import executionRoute from "./routers/executeCode.route.js";
 import cookieParser from "cookie-parser";
 import submissionRoute from "./routers/submission.route.js";
 import playlistRouter from "./routers/playlist.route.js";
+import profileRouter from "./routers/profile.route.js";
 import dotenv from "dotenv";
 
 dotenv.config("../");
@@ -29,5 +30,6 @@ app.use("/api/v1/problems", problemRoute);
 app.use("/api/v1/execute-code", executionRoute);
 app.use("/api/v1/submission", submissionRoute);
 app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/profile", profileRouter);
 
 export default app;
