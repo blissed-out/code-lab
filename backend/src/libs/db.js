91ced9dab31c70bf8a34aea66a211360ab07521e
blissed-out <aadarshbanjade.computer@gmail.com>
2025-06-03 20:22:51 +0545

    security: omit password field by default from queries

--- backend/src/libs/db.js
+++ backend/src/libs/db.js
@@ -2,6 +2,18 @@ import { PrismaClient } from "../generated/prisma/index.js";
 
 const globalForPrisma = globalThis;
 
-export const db = globalForPrisma.prisma || new PrismaClient();
+export const db =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    omit: {
+      user: {
+        password: true,
+        emailToken: true,
+        tokenExpiry: true,
+        passwordToken: true,
+        passwordTokenExpiry: true,
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
