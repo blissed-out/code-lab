97ea72de92f72a799612baefd12b4a60e80fc1a6
blissed-out <aadarshbanjade.computer@gmail.com>
2025-06-03 09:04:23 +0545

    refactor: move sumbit code to submission store

--- frontend/src/store/useExecutionStore.js
+++ frontend/src/store/useExecutionStore.js
@@ -4,37 +4,7 @@ import toast from "react-hot-toast";
 
 export const useExecutionStore = create((set) => ({
   isExecuting: false,
-  submission: null,
   execution: null,
-
-  submitCode: async (
-    source_code,
-    language_id,
-    stdin,
-    expected_outputs,
-    problemId,
-  ) => {
-    try {
-      set({ isExecuting: true });
-
-      const res = await axiosInstance.post("/submission/submit-code", {
-        source_code,
-        language_id,
-        stdin,
-        expected_outputs,
-        problemId,
-      });
-
-      set({ submission: res.data.data });
-
-      toast.success(res.data.message);
-    } catch (error) {
-      console.error("Error executing code", error);
-      toast.error("Error executing code");
-    } finally {
-      set({ isExecuting: false });
-    }
-  },
 
   runCode: async (source_code, language_id, stdin, expected_outputs) => {
     try {
