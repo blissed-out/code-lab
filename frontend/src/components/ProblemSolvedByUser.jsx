e4596e85e08c804698b4f7a49bf107b80e836c2a
blissed-out <aadarshbanjade.computer@gmail.com>
2025-06-07 14:02:36 +0545

    fix: route to home page for problem
    temporary fix

--- frontend/src/components/ProblemSolvedByUser.jsx
+++ frontend/src/components/ProblemSolvedByUser.jsx
@@ -120,7 +120,7 @@ const ProblemSolvedByUser = () => {
                   Total problems solved:{" "}
                   <span className="font-bold">{solvedProblems.length}</span>
                 </span>
-                <Link to="/problems" className="btn btn-sm btn-primary">
+                <Link to="/" className="btn btn-sm btn-primary">
                   Solve more problems
                 </Link>
               </div>
